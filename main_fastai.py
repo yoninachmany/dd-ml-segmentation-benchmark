c872e5975190e2c6ef5595a867d548fea67cd8cc
Yoni Nachmany <yoni.nachmany@mapbox.com>
2020-05-25 10:47:29 -0400

    only download, no wandb

--- main_fastai.py
+++ main_fastai.py
@@ -15,22 +15,22 @@ if __name__ == '__main__':
         'dataset' : dataset,
     }
 
-    wandb.init(config=config)
+    # wandb.init(config=config)
 
     datasets.download_dataset(dataset)
 
-    # train the baseline model and save it in models folder
-    training.train_model(dataset)
-
-    # use the train model to run inference on all test scenes
-    inference.run_inference(dataset)
-
-    # scores all the test images compared to the ground truth labels then
-    # send the scores (f1, precision, recall) and prediction images to wandb
-    score, predictions = scoring.score_predictions(dataset)
-    print(score)
-    wandb.log(score)
-
-    for f1, f2 in predictions:
-        wandb.save( f1 )
-        wandb.save( f2 )
+    # # train the baseline model and save it in models folder
+    # training.train_model(dataset)
+    #
+    # # use the train model to run inference on all test scenes
+    # inference.run_inference(dataset)
+    #
+    # # scores all the test images compared to the ground truth labels then
+    # # send the scores (f1, precision, recall) and prediction images to wandb
+    # score, predictions = scoring.score_predictions(dataset)
+    # print(score)
+    # wandb.log(score)
+    #
+    # for f1, f2 in predictions:
+    #     wandb.save( f1 )
+    #     wandb.save( f2 )
